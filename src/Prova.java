448fab23eaabec99d170ac3e9cc2d1353acff4b2
GMB <cukden@gmail.com>
2015-12-07 16:30:11 +0100

    Prima commit

---
+++ src/Prova.java
@@ -0,0 +1,28 @@
+import lombok.Cleanup;
+import lombok.Data;
+
+/**
+ * Created by Loris on 06/12/2015.
+ */
+
+
+@Data
+public class Prova implements Runnable {
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+        try {
+            Thread.sleep(3000);
+        } catch(InterruptedException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("booom!");
+    }
+
+    public static void main(String args[]) {
+        Thread t = new Thread(new Prova());
+        t.start();
+    }
+
+}
