9a28b6afb0c637c3ffe1babc9883142a22ff0f76
GMB <cukden@gmail.com>
2015-12-13 20:24:21 +0100

    Prima commit

--- src/Prova.java
+++ src/Prova.java
@@ -1,5 +1,8 @@
+import com.google.common.base.Stopwatch;
 import lombok.Cleanup;
 import lombok.Data;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Loris on 06/12/2015.
@@ -16,6 +19,7 @@ public class Prova implements Runnable {
             while(i<0.5)
             {
                 i=Math.random();
+                System.out.println("Estratto numero: "+i);
                 Thread.sleep(1000);
             }
         } catch(InterruptedException e)
@@ -28,6 +32,16 @@ public class Prova implements Runnable {
     public static void main(String args[]) {
         Thread t = new Thread(new Prova());
         t.start();
+
+
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        long ms = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+        while(ms<1000)
+        {}
+        System.out.println("ASPETTO");
+
+        stopwatch.stop();
+
     }
 
 }
